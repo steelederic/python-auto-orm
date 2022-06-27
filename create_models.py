b6a353099ed93f6456f3f282b662c0308a4aef14
Eric Steele <steelederic@gmail.com>
2022-06-27 13:56:52 -0400

    Fix import in create_models.py

--- create_models.py
+++ create_models.py
@@ -1,5 +1,5 @@
 import os
 import subprocess
-from app import connection_string
+from config import connection_string
 
 subprocess.run(f"sqlacodegen {connection_string} --noviews --outfile models.py", capture_output=True)
